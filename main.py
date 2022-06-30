a6a0d11e3137b9726f70ce51e6239ef0270bd93c
Yedrald Valerín Pérez <yedrald@hotmail.com>
2022-06-29 22:50:13 -0600

    Update main.py
    Removed not necessary imports

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 from classes.product import Product
-from data.constants import PRODUCTS_PATH, RESULT_PATH, TEST_REPLACEMENT_DATA
-from modules.csv_module import read_csv
+from data.constants import PRODUCTS_PATH
 from modules.image_module import generate_images
 
 def execute():
     csv_file = Path(PRODUCTS_PATH)
-    result_path = Path(RESULT_PATH)
     products = [p for p in Product.get_products(csv_file) if not p.created]
     generate_images(products)
     
